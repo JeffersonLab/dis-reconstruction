007294e5d1d67473a79a700c39219d550f7d83f3
Barak Schmookler <bschmookler1@gmail.com>
2021-06-04 18:29:37 -0400

    Update

--- pythia/smear_matrix/smear_5_41.sh
+++ pythia/smear_matrix/smear_5_41.sh
@@ -15,9 +15,13 @@ echo "Performing Job $1"
 echo "..."
 echo ""
 
+VAR1=$(($1+0))
+
 echo "Making Output ROOT File..."
-#root -l -b -q "make_smeared_matrix.C(\"outfiles/yellow_report/5_41\",\"ep_5_41_newtune_$1\")" #This works too
-root -l -b -q 'make_smeared_matrix.C("outfiles/yellow_report/5_41","ep_5_41_newtune_'$1'")'
+#root -l -b -q "make_smeared_matrix.C(\"outfiles/yellow_report/5_41\",\"ep_5_41_newtune_${VAR1}\")" #This works too
+#root -l -b -q 'make_smeared_matrix.C("outfiles/yellow_report/5_41","ep_5_41_newtune_'${VAR1}'")'
+#root -l -b -q 'make_smeared_matrix.C("outfiles/other_studies/5_41/higher_Q2","ep_5_41_newtune_'${VAR1}'")'
+root -l -b -q 'make_smeared_matrix.C("outfiles/other_studies/5_41","ep_minbias_'${VAR1}'")'
 echo "Done!!!"
 echo ""
 
